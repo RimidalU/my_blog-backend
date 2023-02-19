33049aa8b085504ad0fe950f306cf0e88e7dd31e
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-20 01:00:00 +0300

    feat: add base login rout

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send('Hello, My Blog!')
 })
 
+app.post('/auth/login', (req, res) => {
+  console.log(req.body);
+  res.json({
+    success: true,
+  })
+})
+
 app.listen(4000, (err) => {
-  if(err) {
+  if (err) {
     return console.log({ err })
   }
   console.log('server start on port 4000')
