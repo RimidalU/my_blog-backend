4a504be7cde2a3611b15cb5d4ba9111a6cb0fc9e
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-20 11:12:06 +0300

    feat: implement user register validation request

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
 import * as dotenv from 'dotenv'
+import { validationResult } from 'express-validator'
+
+import { registerValidation } from './validations/auth.js'
+
 dotenv.config()
 
 mongoose.connect(process.env.MONGODB_CONNECT_LINK)
-.then(() => {console.log('MongoDB connect OK!')})
-.catch((err) => {console.log(`MongoDB connect Error: ${err}`)})
+  .then(() => { console.log('MongoDB connect OK!') })
+  .catch((err) => { console.log(`MongoDB connect Error: ${err}`) })
 
 const port = process.env.PORT_OF_SERVER || 3600
 
@@ -17,11 +21,17 @@ app.get('/', (req, res) => {
   res.send('Hello, My Blog!')
 })
 
-app.post('/auth/register', (req, res) => {
-
+app.post('/auth/register', registerValidation, (req, res) => {
+  const errors = validationResult(req)
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array())
+  }
+  res.json({
+    success: true
+  })
 })
 
-app.listen(port , (err) => {
+app.listen(port, (err) => {
   if (err) {
     return console.log({ err })
   }
---
+++ validations/auth.js
@@ -0,0 +1,8 @@
+import { body } from 'express-validator'
+
+export const registerValidation = [
+  body('email', 'Incorrect Email format').isEmail(),
+  body('password', 'Password must be at least 5 characters in length').isLength({ min: 5 }),
+  body('fullName', 'Insert full name').isLength({ min: 3 }),
+  body('avatarUrl', 'Wrong avatar link').optional().isURL(),
+]
