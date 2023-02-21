0ebfbb22a5743130222708718f8a22045f49483f
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-21 15:51:07 +0300

    feat: implement database user registration

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import mongoose from 'mongoose'
+import bcrypt from 'bcrypt'
 import * as dotenv from 'dotenv'
 import { validationResult } from 'express-validator'
 
+import UserModel from './models/User.js'
 import { registerValidation } from './validations/auth.js'
 
 dotenv.config()
@@ -21,14 +23,38 @@ app.get('/', (req, res) => {
   res.send('Hello, My Blog!')
 })
 
-app.post('/auth/register', registerValidation, (req, res) => {
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array())
+app.post('/auth/register', registerValidation, async (req, res) => {
+
+  try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array())
+    }
+
+    const password = req.body.password
+    const salt = await bcrypt.genSalt(10)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    const newUserDoc = new UserModel({
+      fullName: req.body.fullName,
+      email: req.body.email,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    })
+
+    const user = await newUserDoc.save()
+
+    res.status(200).json({
+      success: true,
+      user
+    })
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Failed to register user ${req.body.fullName}`,
+      error
+    })
   }
-  res.json({
-    success: true
-  })
 })
 
 app.listen(port, (err) => {
