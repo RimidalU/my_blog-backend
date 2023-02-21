79b6998ddbd656ca2bb8ef9ae5064bf764821fc2
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-21 22:59:13 +0300

    feat: implement upload image route

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -24,32 +24,117 @@ export const create = async (req, res) => {
 
 export const getOne = async (req, res) => {
   try {
+    const postId = req.params.id
 
+    PostModel.findOneAndUpdate({
+      _id: postId
+    }, {
+      $inc: { viewsCount: 1 },  //increase view counter
+    }, {
+      returnDocument: 'after'
+    },
+      (error, data) => {
+        if (error) {
+          return res.status(500).json({
+            success: false,
+            message: `Failed to get the post`,
+            error
+          })
+        }
+
+        if (!data) {
+          return res.status(404).json({
+            success: false,
+            message: `The post not found`
+          })
+        }
+
+        res.status(200).json(data)
+      }
+    )
   } catch (error) {
-
+    res.status(500).json({
+      success: false,
+      message: `Failed to get the post`,
+      error
+    })
   }
 }
 
 export const remove = async (req, res) => {
   try {
+    const postId = req.params.id
 
+    PostModel.findOneAndDelete({
+      _id: postId,
+    },
+      (error, data) => {
+
+        if (error) {
+          return res.status(500).json({
+            success: false,
+            message: 'Failed to remove the post.',
+            error
+          })
+        }
+
+        if (!data) {
+          return res.status(404).json({
+            success: false,
+            message: 'The post not found.'
+          })
+        }
+
+        res.status(200).json({
+          success: true,
+        })
+      })
   } catch (error) {
-
+    res.status(500).json({
+      success: false,
+      message: 'Failed to remove post.',
+      error
+    })
   }
 }
 
 export const update = async (req, res) => {
   try {
+    const postId = req.params.id
 
+    await PostModel.updateOne({
+      _id: postId,
+    },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        tags: req.body.tags,
+        author: req.userId,
+      })
+
+    res.status(200).json({
+      success: true
+    })
   } catch (error) {
-
+    res.status(500).json({
+      success: false,
+      message: 'Failed to update post.',
+      error
+    })
   }
 }
 
 export const getAll = async (req, res) => {
   try {
+    const posts = await PostModel.find().populate({ path: 'author', select: ['fullName', '_id'] }).exec()
 
+    res.status(200).json(posts)
   } catch (error) {
-
+    res.status(500).json({
+      success: false,
+      message: `Failed to get posts.`,
+      error
+    })
   }
 }
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import multer from 'multer'
 import * as dotenv from 'dotenv'
 
 import * as UserController from './controllers/UserController.js'
@@ -18,6 +19,18 @@ mongoose.connect(process.env.MONGODB_CONNECT_LINK)
 const port = process.env.PORT_OF_SERVER || 3600
 
 const app = express()
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage })
+
 app.use(express.json())
 
 app.get('/', (req, res) => {
@@ -28,12 +41,17 @@ app.post('/auth/register', registerValidation, UserController.register)
 app.post('/auth/login', loginValidation, UserController.login)
 app.get('/auth/me', checkAuth, UserController.getMe)
 
-// app.get('/posts', PostController.getAll)
-// app.get('/posts/:id', PostController.getOne)
+app.get('/posts', PostController.getAll)
+app.get('/posts/:id', checkAuth, PostController.getOne)
 app.post('/posts', checkAuth, postCreateValidation, PostController.create)
-// app.delete('/posts', PostController.remove)
-// app.patch('/posts', PostController.update)
+app.delete('/posts/:id', checkAuth, PostController.remove)
+app.patch('/posts/:id', checkAuth, PostController.update)
 
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.status(200).json({
+    url: `/uploads/${req.file.originalname}`
+  })
+})
 
 app.listen(port, (err) => {
   if (err) {
