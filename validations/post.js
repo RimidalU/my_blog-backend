2139106efa76a2f0f0291889972f21b4c6282e82
Uladzimir Stankevich <stankevichuuladimiru@gmail.com>
2023-02-22 19:28:53 +0300

    fix: refactor type of post field

--- validations/post.js
+++ validations/post.js
@@ -3,6 +3,6 @@ import { body } from 'express-validator'
 export const postCreateValidation = [
   body('title', 'Enter the title of the article').isLength({ min: 3 }).isString(),
   body('text', 'Enter the text of the article').isLength({ min: 5 }).isString(),
-  body('tags', 'Invalid tag format (specify array)').optional().isString(),
+  body('tags', 'Invalid tag format (specify array)').optional().isArray(),
   body('imageUrl', 'Wrong image link').optional().isString(),
 ]
